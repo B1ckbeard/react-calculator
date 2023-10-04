fff77bf7aaeeaf4c2db3a0f0e1d393cbe864727d
B1ckbeard <46893211+B1ckbeard@users.noreply.github.com>
2023-10-04 23:17:21 +0300

    calc 0.1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
+  const [input, setInput] = React.useState('');
+  const calcBtns = [];
+  [7, 8, 9, 4, 5, 6, 1, 2, 3, 0, "."].forEach((item) => {
+    calcBtns.push(
+      <Button variant="light"
+        onClick={(e) => {
+          setInput(input + e.target.value);
+        }}
+        value={item}
+        key={item}>{" "}{item}
+      </Button>
+    );
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div class="calculator card">
+      <input type="text" class="calculator-screen z-depth-1" value={input} disabled />
+      <div class="calculator-keys">
+
+        {/* add button */}
+        <Button variant="outline-success" onClick={(e) => setInput(input + e.target.value)} value="+">
+          +
+        </Button>
+
+        {/* minus btn */}
+        <Button variant="outline-success" onClick={(e) => setInput(input + e.target.value)} value="-">
+          -
+        </Button>
+
+        {/* *-btn */}
+        <Button variant="outline-success" onClick={(e) => setInput(input + e.target.value)} value="*">
+          *
+        </Button>
+
+        {/* /-btn */}
+        <Button variant="outline-success" onClick={(e) => setInput(input + e.target.value)} value="/">
+          /
+        </Button>
+        {calcBtns}
+
+        {/* clear all */}
+        <Button variant="outline-danger"
+          onClick={() => setInput("")} value="">
+          AC
+        </Button>
+
+        {/* "=" btn */}
+        <Button variant="outline-dark" className="equal-sign"
+          onClick={(e) => {
+            try {
+              setInput(
+                String(eval(input)).length > 3 &&
+                  String(eval(input)).includes(".")
+                  ? String(eval(input).toFixed(4))
+                  : String(eval(input))
+              );
+            } catch (e) {
+              console.log(e);
+            }
+          }}
+          value="="
         >
-          Learn React
-        </a>
-      </header>
+          =
+        </Button>
+      </div>
     </div>
   );
 }
